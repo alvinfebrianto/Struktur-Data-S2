362b00a669eaf3704418fd437df3c0f984999319
alvinfebrianto <90168547+alvinfebrianto@users.noreply.github.com>
2022-03-01 18:42:44 +0700

    Create AlvinFebrianto_031_[3].cpp

---
+++ Praktikum 2/AlvinFebrianto_031_[3].cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	int matA[2][2] = {4, 2, 8, 10};
+	int matB[2][2] = {-5, 8, 4, -12};
+	int j,k;
+	
+	cout << "Matriks A" << endl;
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			cout << matA[i][j] << " ";
+		}
+	cout << endl;
+	}
+	
+	cout << "\nMatriks B" << endl;
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			cout << matB[i][j] << " ";
+		}
+	cout << endl;
+	}
+	
+	cout << "\nHasil perkalian matriks A dengan eksponen 5 " << endl;
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			cout << matA[i][j] * 5 << " ";
+		}
+	cout << endl;
+	}
+	
+	getch();
+}
