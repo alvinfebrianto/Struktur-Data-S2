ad9f34a1154f77f4527c8e91fa0aba4a3de2c18e
alvinfebrianto <90168547+alvinfebrianto@users.noreply.github.com>
2022-04-12 23:55:54 +0700

    Update AlvinFebrianto_031_Fibonacci (Recursive).cpp

--- Pertemuan 9/AlvinFebrianto_031_Fibonacci (Recursive).cpp
+++ Pertemuan 9/AlvinFebrianto_031_Fibonacci (Recursive).cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 // Membuat fungsi fibonacci    
 int Fibonacci(int n) {
-	  // Jika variable n bernilai 0 atau 1 maka akan mengembalikan nilai n
+    // Jika variable n bernilai 0 atau 1 maka akan mengembalikan nilai n
     if (n == 0 || n == 1) {
         return n;
       // Jika variable n bernilai selain 0 dan 1 maka akan melakukan operasi rekursif
     } else {
-    	  return(Fibonacci(n-1) + Fibonacci(n-2));
+    	return(Fibonacci(n-1) + Fibonacci(n-2));
       }
     }
     
@@ -18,19 +18,19 @@ int main() {
     int n = 0, batas;
     cout << "Program Fibonacci \n\n";
     // Input nilai ke dalam program
-  	cout << "Masukkan jumlah bilangan fibonacci: "; cin >> batas;
-  	cout << "\n";
-  	// Melakukan looping terhadap nilai yang telah diinput
+    cout << "Masukkan jumlah bilangan fibonacci: "; cin >> batas;
+    cout << "\n";
+    // Melakukan looping terhadap nilai yang telah diinput
     for (int i = 1; i <= batas; i++) {
-		cout << Fibonacci(n);
+	cout << Fibonacci(n);
         n++;
         // Menampilkan tanda koma atau titik setelah bilangan fibonacci
         if(i < batas) {
-          	cout << ", ";
-		    } else {
-			      cout << ".";
-	    	}
+            cout << ", ";
+        } else {
+	    cout << ".";
+	}
     }
    
-	getch();
+    getch();
 }
