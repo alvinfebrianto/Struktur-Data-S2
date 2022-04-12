a1c163a16d0027c5c52896ea9311b14413bda11c
alvinfebrianto <90168547+alvinfebrianto@users.noreply.github.com>
2022-04-13 00:02:08 +0700

    Update AlvinFebrianto_031_Fibonacci (Looping).cpp

--- Pertemuan 9/AlvinFebrianto_031_Fibonacci (Looping).cpp
+++ Pertemuan 9/AlvinFebrianto_031_Fibonacci (Looping).cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-	// Deklarasi variabel
-	int n, f_n1 = 0, f_n2 = 1, f_n = 0;
+    // Deklarasi variabel
+    int n, f_n1 = 0, f_n2 = 1, f_n = 0;
 	
-	cout << "Program Fibonacci \n\n";
-	// Input nilai ke dalam program
-	cout << "Masukkan nilai ke-n: "; cin >> n;	
+    cout << "Program Fibonacci \n\n";
+    // Input nilai ke dalam program
+    cout << "Masukkan nilai ke-n: "; cin >> n;	
 	
-	// Melakukan looping terhadap nilai yang telah diinput
-	for(int i = 1; i <= n; i++) {
-		    // Mencetak dua deret pertama bilangan fibonacci
+    // Melakukan looping terhadap nilai yang telah diinput
+    for(int i = 1; i <= n; i++) {
+	// Mencetak dua deret pertama bilangan fibonacci
         if(i == 1) {
             cout << "\n" << f_n1 << ", ";
             continue;
@@ -24,16 +24,16 @@ int main() {
         // Mencetak hasil deret bilangan fibonacci
         f_n = f_n1 + f_n2;
         cout << f_n;
-	    	f_n1 = f_n2;
+	f_n1 = f_n2;
         f_n2 = f_n;
         
         // Menampilkan tanda koma atau titik setelah bilangan fibonacci
         if(i < n) {
-        	  cout << ", ";
-	    	} else {
-		     	  cout << ".";
-	    	}
+            cout << ", ";
+	} else {
+	    cout << ".";
+	}
     }
 	
-	getch();
+    getch();
 }
