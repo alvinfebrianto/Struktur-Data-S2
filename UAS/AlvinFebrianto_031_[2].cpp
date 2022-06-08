9fc67612aa9c4dfe64ccbb587feab963c2731a9b
alvinfebrianto <90168547+alvinfebrianto@users.noreply.github.com>
2022-06-08 19:44:14 +0700

    Update AlvinFebrianto_031_[2].cpp

--- UAS/AlvinFebrianto_031_[2].cpp
+++ UAS/AlvinFebrianto_031_[2].cpp
@@ -4,30 +4,30 @@
 using namespace std;
 
 int main() {
-    char kota_1[10], kota_2[10], kota_3[10], kota_4[10], kota_5[10];
-    int jumlah, panjang, hasil_1, hasil_2, hasil_3, hasil_4, hasil_5, hasil_6, hasil_7;
+	char kota_1[10], kota_2[10], kota_3[10], kota_4[10], kota_5[10];
+	int jumlah, panjang, hasil_1, hasil_2, hasil_3, hasil_4, hasil_5, hasil_6, hasil_7;
     
-    // Input jumlah kota
-    cout << "Jumlah kota dalam Kerajaan Britan: " << endl;
-    cin >> jumlah; cout << endl;
+	// Input jumlah kota
+	cout << "Jumlah kota dalam Kerajaan Britan: " << endl;
+	cin >> jumlah; cout << endl;
     
-    // Input nama kota
-    cout << "- Kota Pertama : "; cin >> kota_1;
-    cout << "- Kota Kedua   : "; cin >> kota_2;
-    cout << "- Kota Ketiga  : "; cin >> kota_3;
-    cout << "- Kota Keempat : "; cin >> kota_4;
+	// Input nama kota
+	cout << "- Kota Pertama : "; cin >> kota_1;
+	cout << "- Kota Kedua   : "; cin >> kota_2;
+	cout << "- Kota Ketiga  : "; cin >> kota_3;
+	cout << "- Kota Keempat : "; cin >> kota_4;
 	cout << "- Kota Kelima  : "; cin >> kota_5;
 	cout << endl;
 	
 	// Menampilkan graph yang terjadi
 	cout << "Sisi-sisinya adalah: " << endl;
-    cout << "(" << kota_1 << ", " << kota_2 << "), ";
-    cout << "(" << kota_1 << ", " << kota_4 << "), ";
-    cout << "(" << kota_1 << ", " << kota_5 << "), ";
-    cout << "(" << kota_2 << ", " << kota_3 << "), ";
-    cout << "(" << kota_3 << ", " << kota_5 << "), ";
-    cout << "(" << kota_3 << ", " << kota_4 << "), ";
-    cout << "(" << kota_4 << ", " << kota_5 << ")\n\n";
+	cout << "(" << kota_1 << ", " << kota_2 << "), ";
+	cout << "(" << kota_1 << ", " << kota_4 << "), ";
+	cout << "(" << kota_1 << ", " << kota_5 << "), ";
+	cout << "(" << kota_2 << ", " << kota_3 << "), ";
+	cout << "(" << kota_3 << ", " << kota_5 << "), ";
+	cout << "(" << kota_3 << ", " << kota_4 << "), ";
+	cout << "(" << kota_4 << ", " << kota_5 << ")\n\n";
 	
 	// Menampilkan panjang jalan yang menghubungkan vertex
 	cout << "Panjang jalan antarkota:"<<endl;
@@ -68,7 +68,7 @@ int main() {
 	cout << kota_1 << "-" << kota_4 << "-" << kota_5 << "\n\n";
 	
 	// Total edge yang harus ditempuh
-	cout <<  "Dengan jarak: \n";
+	cout << "Dengan jarak: \n";
 	cout << hasil_2 + hasil_7 << "\n\n";
 	
 	getch();
